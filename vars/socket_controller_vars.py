7b7f3c0e1a81184502de0224ef5069d322a1775d
Andres Andreu <andres@neurofuzzsecurity.com>
2018-03-31 00:11:17 -0400

    socket vars - slight refactor

--- vars/socket_controller_vars.py
+++ vars/socket_controller_vars.py
@@ -3,28 +3,28 @@
     Company: neuroFuzz, LLC
     Date: 10/11/2012
     Last Modified: 08/18/2016
-    
+
     variables to be used by the SocketController class
-    
+
     BSD 3-Clause License
-    
+
     Copyright (c) 2012-2016, Andres Andreu, neuroFuzz LLC
     All rights reserved.
-    
+
     Redistribution and use in source and binary forms, with or without modification,
     are permitted provided that the following conditions are met:
-    
+
     1. Redistributions of source code must retain the above copyright notice,
     this list of conditions and the following disclaimer.
-    
+
     2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation and/or
     other materials provided with the distribution.
-    
+
     3. Neither the name of the copyright holder nor the names of its contributors may
     be used to endorse or promote products derived from this software without specific
     prior written permission.
-    
+
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
     OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -35,13 +35,13 @@
     WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
     OF SUCH DAMAGE.
-    
+
     *** Take note:
     If you use this for criminal purposes and get caught you are on
     your own and I am not liable. I wrote this for legitimate
     pen-testing and auditing purposes.
     ***
-    
+
     Be kewl and give credit where it is due if you use this. Also,
     send me feedback as I don't have the bandwidth to test for every
     condition - Dre
@@ -56,21 +56,21 @@ from string import letters
 ''' based on https://gist.github.com/3962751 '''
 def createTorPassword(secret = ""):
     ind = chr(96)
-    
+
     # for salt
     rng = os.urandom
     # generate salt and append indicator value so that it
     salt = "%s%s" % (rng(8), ind)
-    
+
     prefix = '16:'
     c = ord(salt[8])
-    
+
     EXPBIAS = 6
     count = (16+(c&15)) << ((c>>4) + EXPBIAS)
-    
+
     d = hashlib.sha1()
     tmp = salt[:8]+secret
-    
+
     '''
         hash the salty password as many times as the length of
         the password divides into the count value
@@ -88,7 +88,7 @@ def createTorPassword(secret = ""):
     salt = binascii.b2a_hex(salt[:8]).upper()
     ind = binascii.b2a_hex(ind)
     torhash = binascii.b2a_hex(hashed).upper()
-    
+
     return prefix + salt + ind + torhash
 
 
@@ -148,4 +148,3 @@ def getTorArguments():
 
 def getSocketBounds():
     return(socketLowerBound,socketUpperBound)
-
